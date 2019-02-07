7aebc6b4c36cdc36aea0e76704d39a4c2937dff9
Swastik <swastik@customer.io>
2019-02-07 09:54:27 +0530

    Use name

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,19 @@
 import Route from '@ember/routing/route';
+import { run } from '@ember/runloop';
 
 export default Route.extend({
   model() {
     return this.store.findRecord('template', 1);
   },
-    
+
   setupController(controller, model) {
     this._super(...arguments);
-    setTimeout(() => {
-      model.get('layout.id');
-      setTimeout(() => {
-        model.get('layout.id');
-        setTimeout(() => {
-          model.get('layout.id');
+    run.later(() => {
+      model.get('layout.name');
+      run.later(() => {
+        model.get('layout.name');
+        run.later(() => {
+          model.get('layout.name');
         }, 500);
       }, 500);
     }, 500);
